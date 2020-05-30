604667cd2ec55e5e0805edf5ba9ae7c47396148e
bouncetales <1540281318@qq.com>
2020-05-30 14:45:42 +0800

    新增机器视觉文件

---
+++ mv/build/openimage_autogen/include/ui_mainwindow.h
@@ -0,0 +1,159 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.5
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QPushButton *pushButton_EXIT;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QPushButton *pushButton_OpenImage;
+    QPushButton *pushButton_openvideo;
+    QPushButton *pushButton_opencamera;
+    QPushButton *pushButton_cameratrack;
+    QPushButton *pushButton_approx;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QLabel *label_3;
+    QLabel *label_2;
+    QWidget *tab_2;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(634, 369);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton_EXIT = new QPushButton(centralWidget);
+        pushButton_EXIT->setObjectName(QStringLiteral("pushButton_EXIT"));
+        pushButton_EXIT->setGeometry(QRect(510, 270, 91, 31));
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QStringLiteral("tabWidget"));
+        tabWidget->setGeometry(QRect(0, 0, 621, 271));
+        tab = new QWidget();
+        tab->setObjectName(QStringLiteral("tab"));
+        pushButton_OpenImage = new QPushButton(tab);
+        pushButton_OpenImage->setObjectName(QStringLiteral("pushButton_OpenImage"));
+        pushButton_OpenImage->setGeometry(QRect(10, 20, 91, 51));
+        QFont font;
+        font.setItalic(true);
+        pushButton_OpenImage->setFont(font);
+        pushButton_openvideo = new QPushButton(tab);
+        pushButton_openvideo->setObjectName(QStringLiteral("pushButton_openvideo"));
+        pushButton_openvideo->setGeometry(QRect(10, 90, 91, 51));
+        pushButton_openvideo->setFont(font);
+        pushButton_opencamera = new QPushButton(tab);
+        pushButton_opencamera->setObjectName(QStringLiteral("pushButton_opencamera"));
+        pushButton_opencamera->setGeometry(QRect(10, 154, 91, 51));
+        pushButton_opencamera->setFont(font);
+        pushButton_cameratrack = new QPushButton(tab);
+        pushButton_cameratrack->setObjectName(QStringLiteral("pushButton_cameratrack"));
+        pushButton_cameratrack->setGeometry(QRect(150, 20, 91, 51));
+        pushButton_cameratrack->setFont(font);
+        pushButton_approx = new QPushButton(tab);
+        pushButton_approx->setObjectName(QStringLiteral("pushButton_approx"));
+        pushButton_approx->setGeometry(QRect(150, 90, 91, 51));
+        pushButton_approx->setFont(font);
+        widget = new QWidget(tab);
+        widget->setObjectName(QStringLiteral("widget"));
+        widget->setGeometry(QRect(430, 20, 87, 77));
+        verticalLayout = new QVBoxLayout(widget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(widget);
+        label->setObjectName(QStringLiteral("label"));
+
+        verticalLayout->addWidget(label);
+
+        label_3 = new QLabel(widget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+
+        verticalLayout->addWidget(label_3);
+
+        label_2 = new QLabel(widget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+
+        verticalLayout->addWidget(label_2);
+
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QStringLiteral("tab_2"));
+        tabWidget->addTab(tab_2, QString());
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 634, 22));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        tabWidget->setCurrentIndex(0);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        pushButton_EXIT->setText(QApplication::translate("MainWindow", "EXIT", Q_NULLPTR));
+        pushButton_OpenImage->setText(QApplication::translate("MainWindow", "Open Image", Q_NULLPTR));
+        pushButton_openvideo->setText(QApplication::translate("MainWindow", "Open video", Q_NULLPTR));
+        pushButton_opencamera->setText(QApplication::translate("MainWindow", "Open Camera", Q_NULLPTR));
+        pushButton_cameratrack->setText(QApplication::translate("MainWindow", "Camera track", Q_NULLPTR));
+        pushButton_approx->setText(QApplication::translate("MainWindow", "Approx", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindow", "\347\224\265\344\277\241172", Q_NULLPTR));
+        label_3->setText(QApplication::translate("MainWindow", "3170406056", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MainWindow", "\351\203\221\345\272\267", Q_NULLPTR));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("MainWindow", "image", Q_NULLPTR));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("MainWindow", "Tab 2", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ mv/mainwindow.cpp
@@ -0,0 +1,383 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include "asmOpenCV.h"
+
+#include <iostream> // for standard I/O
+#include <string>   // for strings
+#include <iomanip>  // for controlling float print precision
+#include <sstream>  // string to number conversion
+#include <opencv2/core.hpp>     // Basic OpenCV structures (cv::Mat, Scalar)
+#include <opencv2/imgproc.hpp>  // Gaussian Blur
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>  // OpenCV window I/O
+#include <opencv2/opencv.hpp>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+bool MainWindow::loadFile(const QString &fileName)
+{
+    QImageReader reader(fileName);
+    reader.setAutoTransform(true);
+    const QImage image = reader.read();
+    if (image.isNull()) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                 tr("Cannot load %1.").arg(QDir::toNativeSeparators(fileName)));
+        setWindowFilePath(QString());
+        iv.imageLabel->setPixmap(QPixmap());
+        iv.imageLabel->adjustSize();
+        return false;
+    }
+//! [2] //! [3]
+    iv.imageLabel->setPixmap(QPixmap::fromImage(image));
+//! [3] //! [4]
+    iv.scaleFactor = 1.0;
+
+    //printAct->setEnabled(true);
+    iv.fitToWindowAct->setEnabled(true);
+    iv.updateActions();
+
+    if (!iv.fitToWindowAct->isChecked())
+        iv.imageLabel->adjustSize();
+
+    setWindowFilePath(fileName);
+    iv.show();
+    return true;
+}
+
+bool MainWindow::loadVideo(const QString &fileName)
+{
+//    QImageReader reader(fileName);
+//    reader.setAutoTransform(true);
+//    const QImage image = reader.read();
+    using namespace cv;
+    using namespace std;
+     VideoCapture video(fileName.toStdString());
+     int frameNum = -1;
+    if (!video.isOpened()) {
+        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+                                 tr("Cannot load %1.").arg(QDir::toNativeSeparators(fileName)));
+        setWindowFilePath(QString());
+        iv.imageLabel->setPixmap(QPixmap());
+        iv.imageLabel->adjustSize();
+        return false;
+    }
+    video >> frame;
+    ++frameNum;
+    image = ASM::cvMatToQImage(frame);
+//! [2] //! [3]
+    iv.imageLabel->setPixmap(QPixmap::fromImage(image));
+//! [3] //! [4]
+    iv.scaleFactor = 1.0;
+
+    //printAct->setEnabled(true);
+    iv.fitToWindowAct->setEnabled(true);
+    iv.updateActions();
+
+    if (!iv.fitToWindowAct->isChecked())
+        iv.imageLabel->adjustSize();
+
+    setWindowFilePath(fileName);
+    iv.show();
+    while(1){
+        video >> frame;
+        if (frame.empty())
+                {
+                    cout << " < < <  Game over!  > > > ";
+                    break;
+                }
+        ++frameNum;
+        image = ASM::cvMatToQImage(frame);
+    //! [2] //! [3]
+        iv.imageLabel->setPixmap(QPixmap::fromImage(image));
+    //! [3] //! [4]
+        iv.scaleFactor = 1.0;
+
+        //printAct->setEnabled(true);
+        iv.fitToWindowAct->setEnabled(true);
+        iv.updateActions();
+
+        if (!iv.fitToWindowAct->isChecked())
+            iv.imageLabel->adjustSize();
+
+        setWindowFilePath(fileName);
+        iv.update();
+        char c = (char)waitKey(20);
+    }
+    return true;
+}
+
+bool MainWindow::OpenCamera()
+{
+//    QImageReader reader(fileName);
+//    reader.setAutoTransform(true);
+//    const QImage image = reader.read();
+    using namespace cv;
+    using namespace std;
+     VideoCapture video;
+     int frameNum = -1;
+    if (!video.open(0)) {
+//        QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
+//                                 tr("Cannot load %1.").arg(QDir::toNativeSeparators(fileName)));
+//        setWindowFilePath(QString());
+        cout << "Open Camera failed!"<< endl;
+//        iv.imageLabel->setPixmap(QPixmap());
+//        iv.imageLabel->adjustSize();
+        return false;
+    }
+    video >> frame;
+    ++frameNum;
+    image = ASM::cvMatToQImage(frame);
+//! [2] //! [3]
+    iv.imageLabel->setPixmap(QPixmap::fromImage(image));
+//! [3] //! [4]
+    iv.scaleFactor = 1.0;
+
+    //printAct->setEnabled(true);
+    iv.fitToWindowAct->setEnabled(true);
+    iv.updateActions();
+
+    if (!iv.fitToWindowAct->isChecked())
+        iv.imageLabel->adjustSize();
+
+//    setWindowFilePath(fileName);
+    iv.show();
+    while(1){
+        video >> frame;
+        if (iv.isHidden()){
+            video.release();
+            break;
+        }
+//                {
+//                    cout << " < < <  Game over!  > > > ";
+//                    break;
+//                }
+        ++frameNum;
+        image = ASM::cvMatToQImage(frame);
+    //! [2] //! [3]
+        iv.imageLabel->setPixmap(QPixmap::fromImage(image));
+    //! [3] //! [4]
+        iv.scaleFactor = 1.0;
+
+        //printAct->setEnabled(true);
+        iv.fitToWindowAct->setEnabled(true);
+        iv.updateActions();
+
+        if (!iv.fitToWindowAct->isChecked())
+            iv.imageLabel->adjustSize();
+
+//        setWindowFilePath(fileName);
+        iv.update();
+        char c = (char)waitKey(20);
+    }
+    return true;
+}
+
+void MainWindow::on_pushButton_EXIT_clicked()
+{
+   this->close();
+}
+
+void MainWindow::on_pushButton_OpenImage_clicked()
+{
+      QStringList mimeTypeFilters;
+        foreach (const QByteArray &mimeTypeName, QImageReader::supportedMimeTypes())
+            mimeTypeFilters.append(mimeTypeName);
+        mimeTypeFilters.sort();
+        const QStringList picturesLocations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+        QFileDialog dialog(this, tr("Open File"),
+                           picturesLocations.isEmpty() ? QDir::currentPath() : picturesLocations.last());
+        dialog.setAcceptMode(QFileDialog::AcceptOpen);
+        dialog.setMimeTypeFilters(mimeTypeFilters);
+        dialog.selectMimeTypeFilter("image/jpeg");
+
+        while (dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().first())) {}
+
+}
+
+void MainWindow::on_pushButton_openvideo_clicked()
+{
+    QStringList mimeTypeFilters;
+//      foreach (const QByteArray &mimeTypeName, QImageReader::supportedMimeTypes())
+          mimeTypeFilters.append("video/avi");
+      mimeTypeFilters.sort();
+      const QStringList picturesLocations = QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+      QFileDialog dialog(this, tr("Open File"),
+                         picturesLocations.isEmpty() ? QDir::currentPath() : picturesLocations.last());
+      dialog.setAcceptMode(QFileDialog::AcceptOpen);
+      dialog.setMimeTypeFilters(mimeTypeFilters);
+      dialog.selectMimeTypeFilter("video/avi");
+
+      while (dialog.exec() == QDialog::Accepted && !loadVideo(dialog.selectedFiles().first())) {}
+}
+
+
+void MainWindow::on_pushButton_opencamera_clicked()
+{
+    OpenCamera();
+}
+
+int MainWindow::on_pushButton_cameratrack_clicked()
+{
+
+    using namespace cv;
+    using namespace std;
+    Mat frame,src;
+    Mat TrackPic=cv::Mat::zeros(500,500,CV_8UC3);
+    char c=0;
+    VideoCapture capture(0);
+    remove("vector.txt");
+    while(1)
+    {
+        capture >> frame;
+        Mat midImage;
+        cvtColor(frame,midImage,CV_BGR2GRAY);
+        GaussianBlur(midImage,midImage,Size(9,9),2,2);
+        vector<Vec3f>circles;
+/*********************************霍夫变换检测圆,画出轮廓、中心点******************************/
+        HoughCircles(midImage,circles,CV_HOUGH_GRADIENT,1.5,100,150,100,0,0);
+
+        for(size_t i=0;i<circles.size();i++)
+        {
+            Point center(cvRound(circles[i][0]),cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
+            circle(frame,center,3,Scalar(0,255,0),-1,8,0);
+            circle(frame,center,radius,Scalar(155,50,255),3,8,0);
+            printf("x=%d,y=%d\n",cvRound(circles[i][0]),cvRound(circles[i][1]));
+/*********************************霍夫变换检测圆,画出轮廓、中心点******************************/
+
+/*************************************存轨迹点*********************************************/
+            std::ofstream fout;
+            fout.open("vector.txt",ios::app);
+            for (int l = 0; l < circles.size(); ++l)
+            {
+                fout << center.x<<' '<< center.y<< endl;
+            }
+            fout.close();
+/*************************************存轨迹点*********************************************/
+        }
+        // namedWindow("1");
+        // imshow("1",TrackPic);
+        // imwrite("1.jpg",TrackPic);
+        namedWindow("result");
+        imshow("result",frame);
+        c =cvWaitKey(30);
+        if(c == 27)
+        {
+            break;
+        }
+    }
+    destroyAllWindows();
+    capture.release();
+    return 0;
+}
+
+int MainWindow::on_pushButton_approx_clicked()
+{
+    using namespace cv;
+    using namespace std;
+    float a,b,c,d,s;
+    Mat TrackPic=cv::Mat::zeros(500,500,CV_8UC3);
+    vector<vector<Point>> contours;
+    vector<Point>p;
+/************************读取保存坐标文件并输出到vector容器中******************************/
+    ifstream outfile;
+    outfile.open("/home/zk/QT/01/openimage/build/vector.txt");
+    Point temp;
+    double temp1=0,temp2=0;
+    while(!outfile.eof())
+    //for(int i=0;!outfile.eof();i++)
+    {
+        outfile>> temp1>>temp2;
+        temp.x=temp1;
+        temp.y=temp2;
+        p.push_back(temp);
+    }
+    contours.push_back(p);
+    // for(int i = 0; i < contours.size(); i++)
+    // {
+    //     cout<<contours[i]<<endl;
+    // }
+    // outfile.close();
+/************************读取保存坐标文件并输出到vector容器中******************************/
+
+
+/******************************逼近拟合、画轮廓*****************************************/
+    vector<vector<Point>> lines(contours.size());
+    for(int i=0;i<contours.size();i++)
+    {
+        approxPolyDP(contours[i],lines[i],30,true);
+        drawContours(TrackPic, lines, i, Scalar(0, 255, 255), 2, 8);  //绘制
+/******************************逼近拟合、画轮廓*****************************************/
+
+
+/**************************************形状判断*******************************************/
+        if(lines[i].size()==2)
+        {
+            vector<Point>m;
+            vector<vector<Point>>::iterator it;
+            vector<Point>::iterator it1;
+            for(it=lines.begin();it!=lines.end();it++)
+            {
+                //cout<<*it<<endl;
+                for(it1=(*it).begin();it1!=(*it).end();it1++)
+                {
+                    //vector<Point>m;
+                    m.push_back(*it1);
+                    //cout<<*it1<<endl;
+                    //cout<<(*it1).x<<','<<(*it1).y<<endl;
+                }
+                //cout<<m[1].x<<endl;
+                a=m[1].x;
+                b=m[0].x;
+                c=m[1].y;
+                d=m[0].y;
+                s=(c-d)/(a-b);
+                if(-1<s && s<1)
+                {
+                    cout<<"水平线"<<endl;
+                }
+                else
+                {
+                    cout<<"垂直线"<<endl;
+                }
+            cout<<s<<endl;
+            }
+        }
+            else if(lines[i].size()==3)
+            {
+                cout<<"三角形"<<endl;
+            }
+            else if(lines[i].size()==4)
+            {
+                cout<<"四边形"<<endl;
+            }
+            else if(lines[i].size()==5)
+            {
+                cout<<"五边形"<<endl;
+            }
+            else if(lines[i].size()>10)
+            {
+                cout<<"圆"<<endl;
+            }
+            else
+            {
+                cout<<"none"<<endl;
+            }
+        cout<<lines[i]<<endl;
+/**************************************形状判断*******************************************/
+    }
+            imshow("approx",TrackPic);
+            waitKey(0);
+            return 0;
+}
